93c4504ea7d8a28b73a5b3c163cb0a1ce54312a2
AlDS90 <lkjs0JH)0fw3vcSFDSF9>
2021-03-04 13:33:05 +0700

    finish

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+import os
 import zipfile
 
 
 def unzip(path, z_path):
     z_file = zipfile.ZipFile(z_path, 'r')
+    for name in z_file.namelist():
+        dir_, file = os.path.split(name)
+        if not os.path.exists(path + os.sep + dir_):
+            os.mkdir(path + os.sep + dir_)
+        if file:
+            handler = open(path + os.sep + name, 'wb')
+            handler.write(z_file.read(name))
+            handler.close()
+    z_file.close()
 
 
-unzip(path, z_path)
+unzip('unarchive_folder', 'archive.zip')
